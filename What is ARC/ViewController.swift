42d0a1c724930e89730cb87516782fb622ba52cf
alonhrri <alonhrri@gmail.com>
2019-03-25 01:29:47 +0200

    first commit

---
+++ What is ARC/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  What is ARC
+//
+//  Created by Alon Harari on 25/03/2019.
+//  Copyright © 2019 Alon Harari. All rights reserved.
+//
+
+
+// ARC = Automatic Reference Counting - this is Apple's way of handling memory managment of objects for you. for each object it is keeping count of how many strong references are poiniting for that object.
+
+//Example:
+//to remove a reference poinitng to an object all we need to do is to set it to nil and the closure deinit will be called.
+//it will be called only if the refernce is weak!!!
+//but if it is a strong reference - deinit wont be called... try and ARC Count will not count down by 1
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+    
+    var alon: Person?
+    var mazda: Car?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createObjects()
+        assignProperties()
+    }
+    func createObjects() {
+        alon = Person(name: "Alon Harari", car: nil) // var alon has a strong refrence to object Person
+        mazda = Car(name: "Mazda 3", owner: nil) // var mazda has a strong refrence to object Car
+        
+        //alon = nil
+        //mazda = nil
+    }
+    func assignProperties() {
+        alon?.car = mazda
+        mazda?.owner = alon
+        
+        alon = nil
+
+    }
+    
+    
+}
+class Person {
+    let name: String
+    var car: Car?
+    
+    init(name: String, car: Car?){
+        self.name = name
+        self.car = car
+    }
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+
+class Car {
+    let name: String
+    weak  var owner: Person?
+    
+    init(name:String, owner: Person?){
+        self.name = name
+        self.owner = owner
+    }
+    deinit {
+        print("Car of owner:\(name) is being deinitialized")
+    }
+}
+
